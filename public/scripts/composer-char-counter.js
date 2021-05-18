71a2cced3787ea75bf087615903fcb1849dc939e
felicia puspita yuwono <feliciaokta@outlook.com>
2021-05-18 15:30:30 -0700

    worked character counter

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,10 @@
+$( document ).ready(function() {
+  $(".tweet-text").on('input', function() {
+    const char = $(this).val();
+    let charLength = char.length;
+    $(".counter").text(140 - charLength);
+
+    (140 - charLength < 0) ? $(".counter").css("color", "red") : $(".counter").css("color", "#4056A1");
+
+  });
+});
