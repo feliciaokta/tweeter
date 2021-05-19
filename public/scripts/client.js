8716874f50ac4af059f8ac312cd71fa70aaebea4
felicia puspita yuwono <feliciaokta@outlook.com>
2021-05-18 20:25:31 -0700

    fixed CSS for posted tweets & added timestamps

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,13 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+$( document ).ready(function() {
+
+  // update the timestamp of posted tweet
+  $(".timePosted").text(timeago.format(new Date()));
+
+  // stick the navbar on top
+  window.onscroll = function() {myFunction()};
+  
+
+});
